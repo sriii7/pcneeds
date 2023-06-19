26fa32ec145c054e5911c487e51ee937675207c9
sriman <78482261+sriii7@users.noreply.github.com>
2023-06-18 22:02:51 -0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -12,3 +12,31 @@ function outFunc() {
   var tooltip = document.getElementById("myTooltip");
   tooltip.innerHTML = "Copy to clipboard";
 }
+
+function myFunction1() {
+  var copyText = document.getElementById("myInput1");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+  
+  var tooltip1 = document.getElementById("myTooltip1");
+  tooltip.innerHTML = "Copied: " + copyText.value;
+}
+function outFunc1() {
+  var tooltip1 = document.getElementById("myTooltip1");
+  tooltip.innerHTML = "Copy to clipboard";
+}
+
+function myFunction2() {
+  var copyText = document.getElementById("myInput2");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+  
+  var tooltip2 = document.getElementById("myTooltip2");
+  tooltip.innerHTML = "Copied: " + copyText.value;
+}
+function outFunc2() {
+  var tooltip2 = document.getElementById("myTooltip2");
+  tooltip.innerHTML = "Copy to clipboard";
+}
